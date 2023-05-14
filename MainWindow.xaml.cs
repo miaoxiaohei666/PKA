045210b86d244e65fea86cfceb91e4e9bf2d6e75
miaoxiaohei <miaoxiaohei666@gmail.com>
2023-05-14 17:38:49 +0800

    init project by xxy

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+
+namespace PKA
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GeneratePublicKey_Click(object sender, RoutedEventArgs e)
+        {
+            // 获取选择的算法和参数值
+            var algorithm = AlgorithmComboBox.SelectedItem.ToString();
+            var keyLength = KeyLengthComboBox.SelectedItem.ToString();
+
+            // 根据选择的算法执行相应的公钥生成逻辑
+            switch (algorithm)
+            {
+                case "RSA":
+                    // 执行RSA算法的公钥生成逻辑
+                    GenerateRSAPublicKey(keyLength);
+                    break;
+                case "ECC":
+                    // 执行ECC算法的公钥生成逻辑
+                    GenerateECCPublicKey();
+                    break;
+                case "ElGamal":
+                    // 执行ElGamal算法的公钥生成逻辑
+                    GenerateElGamalPublicKey();
+                    break;
+                case "Rabin":
+                    // 执行Rabin算法的公钥生成逻辑
+                    GenerateRabinPublicKey();
+                    break;
+                default:
+                    // 默认情况下不执行任何操作
+                    break;
+            }
+        }
+
+        private void GenerateRSAPublicKey(string keyLength)
+        {
+            // TODO: 执行RSA算法的公钥生成逻辑，并将结果展示在PublicKeyTextBox中
+        }
+
+        private void GenerateECCPublicKey()
+        {
+            // TODO: 执行ECC算法的公钥生成逻辑，并将结果展示在PublicKeyTextBox中
+        }
+
+        private void GenerateElGamalPublicKey()
+        {
+            // TODO: 执行ElGamal算法的公钥生成逻辑，并将结果展示在PublicKeyTextBox中
+        }
+
+        private void GenerateRabinPublicKey()
+        {
+            // TODO: 执行Rabin算法的公钥生成逻辑，并将结果展示在PublicKeyTextBox中
+        }
+    }
+}
