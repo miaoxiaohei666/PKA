f34ec6763f6e8a0bb037b8e6497281f0bf4d0a32
miaoxiaohei <miaoxiaohei666@gmail.com>
2023-05-16 15:46:52 +0800

    ecc complete by xxy

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using PKA.PAKCore;
 
@@ -7,30 +6,35 @@ namespace PKA
 {
     public partial class MainWindow : Window
     {
-        
         public MainWindow()
         {
+            RsaParametersPanel = new StackPanel();
+            EccParametersPanel = new StackPanel();
+            ElGamalParametersPanel = new StackPanel();
+            RabinParametersPanel = new StackPanel();
+            
             InitializeComponent();
             ViewModel viewModel = new ViewModel();
             DataContext = viewModel;
+            
         }
 
         private void GeneratePublicKey_Click(object sender, RoutedEventArgs e)
         {
             // 获取选择的算法和参数值
             var algorithm = (AlgorithmComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
-
+            string output;
 
             // 根据选择的算法执行相应的公钥生成逻辑
             switch (algorithm)
             {
                 case "RSA":
-                    var keyLength = int.Parse((KeyLengthComboBox.SelectedItem as ComboBoxItem)?.Content.ToString());
-                    var iterations = int.Parse((IterationsComboBox.SelectedItem as ComboBoxItem)?.Content.ToString());
                     // 执行RSA算法的公钥生成逻辑
-                    var rsaKey = new RsaKey(keyLength, iterations);
+                    var rsaKeyLength = int.Parse((RsaKeyLengthComboBox.SelectedItem as ComboBoxItem)?.Content.ToString() ?? string.Empty);
+                    var iterations = int.Parse((RsaIterationsComboBox.SelectedItem as ComboBoxItem)?.Content.ToString() ?? string.Empty);
+                    var rsaKey = new RsaKey(rsaKeyLength, iterations);
                     rsaKey.GenerateRsaKey();
-                    var output = "KeyLength:\n" + rsaKey.KeyLength.ToString() + "\n";
+                    output = "KeyLength:\n" + rsaKey.KeyLength.ToString() + "\n";
                     output += "Iterations:\n" + rsaKey.Iterations.ToString() + "\n";
                     output += "Modulus:\n" + rsaKey.Modulus.ToString() + "\n";
                     output += "PublicExponent:\n" + rsaKey.PublicExponent.ToString() + "\n";
@@ -39,19 +43,57 @@ namespace PKA
                     break;
                 case "ECC":
                     // 执行ECC算法的公钥生成逻辑
-                   
+                    var eccKeyLength = int.Parse((EccKeyLengthComboBox.SelectedItem as ComboBoxItem)?.Content.ToString() ?? string.Empty);
+                    var eccKey = new EccKey(eccKeyLength);
+                    eccKey.GenerateEccKey();
+                    output = "KeyLenth:\n" + eccKey.EccAlgorithm.KeySize.ToString() + "\n";
+                    output = "PrivateKey:\n" + eccKey.PrivateKey.ToString() + "\n";
+                    output = "PublicKey:\n" + eccKey.PublicKey.ToString() + "\n";
+                    OutputBox.Text = output;
                     break;
                 case "ElGamal":
                     // 执行ElGamal算法的公钥生成逻辑
-                   
+
                     break;
                 case "Rabin":
                     // 执行Rabin算法的公钥生成逻辑
-                   
+
                     break;
                 default:
                     // 默认情况下不执行任何操作
+                    OutputBox.Text = "Error!";
                     break;
+            }
+        }
+
+        private void AlgorithmComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // 根据选择的选项，设置相应的控件元素可见性
+            string? selectedOption = (AlgorithmComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
+            
+            RsaParametersPanel.Visibility = Visibility.Collapsed;
+            EccParametersPanel.Visibility = Visibility.Collapsed;
+            ElGamalParametersPanel.Visibility = Visibility.Collapsed;
+            RabinParametersPanel.Visibility = Visibility.Collapsed;
+
+            if (selectedOption != null)
+            {
+                if (selectedOption == "RSA")
+                {
+                    RsaParametersPanel.Visibility = Visibility.Visible;
+                }
+                else if (selectedOption == "ECC")
+                {
+                    EccParametersPanel.Visibility = Visibility.Visible;
+                }
+                else if (selectedOption == "ElGamal")
+                {
+                    ElGamalParametersPanel.Visibility = Visibility.Visible;
+                }
+                else if (selectedOption == "Rabin")
+                {
+                    RabinParametersPanel.Visibility = Visibility.Visible;
+                }
             }
         }
     }
---
+++ PAKCore/EccKey.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Numerics;
+using System.Security.Cryptography;
+
+namespace PKA.PAKCore
+{
+    public class EccKey
+    {
+        private ECDsa _eccAlgorithm;
+        private BigInteger _publicKey;
+        private BigInteger _privateKey;
+
+        public EccKey(int keyLength)
+        {
+            _eccAlgorithm = ECDsa.Create(); // 创建 ECC 算法实例
+            _eccAlgorithm.KeySize = keyLength;
+        }
+
+        public void GenerateEccKey()
+        {
+            _eccAlgorithm.GenerateKey(ECCurve.NamedCurves.nistP256); // 选择椭圆曲线参数
+
+            // 获取生成的公钥和私钥
+            var publicKey = _eccAlgorithm.ExportSubjectPublicKeyInfo();
+            var privateKey = _eccAlgorithm.ExportPkcs8PrivateKey();
+
+            // 将公钥和私钥转换为 BigInteger
+            _publicKey = new BigInteger(publicKey);
+            _privateKey = new BigInteger(privateKey);
+        }
+        
+        public ECDsa EccAlgorithm
+        {
+            get => _eccAlgorithm;
+            set => _eccAlgorithm = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public BigInteger PublicKey
+        {
+            get => _publicKey;
+            set => _publicKey = value;
+        }
+
+        public BigInteger PrivateKey
+        {
+            get => _privateKey;
+            set => _privateKey = value;
+        }
+    }
+}
